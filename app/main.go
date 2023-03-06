7b1960369f935700406bb4d5da38155874bcda65
Kurichi <yuuyapp080@gmail.com>
2023-03-06 22:25:24 +0900

    [add] graceful shutdown

--- app/main.go
+++ app/main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"app/api/router"
+	"context"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
 	e := router.NewRouter()
 
 	e.Logger.Fatal(e.Start(":8080"))
+
+	// Graceful shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
